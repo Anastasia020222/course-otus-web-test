ef9e8dc97a5117ea2a7a17e032e9486b0f18dc55
Nastya <stasyyyaa@mail.ru>
2024-04-08 15:51:18 +0300

    add pipeline

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -1,26 +1,40 @@
 timeout(60) {
     node("maven-slave") {
+
+        wrap([$class: 'BuildUser']) {
+            currentBuild.description = """
+                build user : $BUILD_USER
+                branch : $REFSPEC
+            """
+
+            params = readYaml text: env.YAML_CONFIG ?: null
+            if (params != null) {
+                for (param in params.entrySet()) {
+                    env.setProperty(param.getKey(), param.getValue)
+                }
+            }
+        }
+
         stage("Checkout") {
             checkout scm
         }
-        stage("Run tests") {
-            def exitCode = sh(
-                    returnStatus: true,
-                    script: """
-                    mvn test -Dbrowser=$BROWSER -Dbrowser.version=$BROWSER_VERSION -Dwebdriver.base.url=$BASE_URL -Dwebdriver.remote.url=$GRID_URL
-                    """
-            )
-            if(exitCode == 1) {
-                currentBuild.result = 'UNSTABLE'
-            }
+
+        stage("") {
+            sh "echo BROWSER=${env.getProperty('BROWSER')} > ./.env"
+            sh "echo BROWSER_VESION=${env.getProperty('BROWSER_VESION')} >> ./.env"
+            sh "echo REMORE_URL=${env.getProperty('REMORE_URL')} >> ./.env"
         }
+
+        stage("Run UI tests") {
+            //sh ("mvn test -Dbrowser=$BOWSER -Dbrowser_version=$BROWSER_VESION -Dremote=$REMORE_URL")
+            sh("mkdir ./reports")
+            sh "docker run --rm --env-file -v ./report:/root/ui_tests/allure-result ./ ./.env -t ui_tests:1.0.0"
+        }
+
         stage("Publish allure results") {
             allure([
-                    includeProperties: false,
-                    jdk: '',
-                    properties: [],
                     reportBuildPolicy: 'ALWAYS',
-                    results: [[path: 'allure-results']]
+                    results          : ['./reports']
             ])
         }
     }
