a003b76b8e9b9ec36704d81f7bffbcc3d9f98e2e
Nastya <stasyyyaa@mail.ru>
2024-04-10 15:17:36 +0300

    add stage build docker

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -31,6 +31,7 @@ timeout(60) {
         stage("Build Docker image") {
             // Сборка Docker-образа
             sh 'pwd'
+            echo "Workspace: ${env.WORKSPACE}"
             dir('/home/unixuser/ui_tests') {
                 sh "docker build -t ui_tests:1.0.0 ."
             }
