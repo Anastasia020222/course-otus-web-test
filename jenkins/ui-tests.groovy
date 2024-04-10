44d55502da0e14f64bd8240aeeb923fd9ae94e64
Nastya <stasyyyaa@mail.ru>
2024-04-10 14:10:46 +0300

    add stage build docker

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -23,20 +23,25 @@ timeout(60) {
 
         stage("Create configurations") {
             sh "echo BASE_URL=${env.getProperty('BASE_URL')} > ./.env"
-            sh "echo BROWSER=${env.getProperty('BROWSER')} > ./.env"
+            sh "echo BROWSER=${env.getProperty('BROWSER')} >> ./.env"
             sh "echo VERSION_BROWSER=${env.getProperty('VERSION_BROWSER')} >> ./.env"
             sh "echo REMOTE=${env.getProperty('REMOTE')} >> ./.env"
         }
 
         stage("Build Docker image") {
             // Сборка Docker-образа
-            sh "docker build -t ui_tests:1.0.0 ."
+            echo pwd
+            dir('/home/unixuser/ui_tests') {
+                sh "docker build -t ui_tests:1.0.0 ."
+            }
         }
 
 
         stage("Run UI tests") {
             sh("mkdir ./reports")
-            sh "docker run --rm --env-file -v ./reports:/root/ui_tests/allure-result ./ ./.env -t ui_tests:1.0.0"
+            sh "docker run --rm --env-file ./.env -v ./reports:/root/ui_tests/allure-result ui_tests:1.0.0"
+
+            // sh "docker run --rm --env-file -v ./reports:/root/ui_tests/allure-result ./ ./.env -t ui_tests:1.0.0"
         }
 
         stage("Publish allure results") {
