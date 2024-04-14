a95c5640f4a53a634c8b6c267876c6abe01bdd70
Nastya <stasyyyaa@mail.ru>
2024-04-14 12:56:34 +0300

    add allure

--- src/main/java/org/example/common/Constants.java
+++ src/main/java/org/example/common/Constants.java
@@ -5,5 +5,5 @@ public class Constants {
   public final static String CHROME = "chrome";
   public final static String FIREFOX = "firefox";
   public final static String OPERA = "opera";
-  public final static String SELENIUM_ADDRESS = "http://127.0.0.1:80/wd/hub"; //172.30.207.187
+  public final static String SELENIUM_ADDRESS = "http://172.30.207.187:80/wd/hub"; //172.30.207.187
 }
