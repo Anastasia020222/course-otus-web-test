b883ff9945c1eb7c6bc0d23102d17554eb51b172
Nastya <stasyyyaa@mail.ru>
2024-04-14 16:37:42 +0300

    add

--- jenkins/ui-tests.groovy
+++ jenkins/ui-tests.groovy
@@ -35,7 +35,7 @@ timeout(60) {
         try {
             stage("Run UI tests") {
                 sh("mkdir ./reports")
-                sh "docker run --rm --env-file ./.env -v /home/jenkins/workspace/ui-tests/reports:/home/unixuser/ui_tests/target/allure-results -t ui_tests:1.0.0"
+                sh "docker run --rm --env-file ./.env -v /home/jenkins/workspace/ui-tests/reports:/home/unixuser/ui_tests/allure-results -t ui_tests:1.0.0"
             }
         }
         finally {
